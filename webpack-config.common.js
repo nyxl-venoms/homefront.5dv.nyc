8a5b27f450fec2eccf7884346cda7c9ba4bb4122
Uchendu Nwachuku <developer@unndunn.com>
2019-12-10 13:00:34 -0500

    Implement vertical rhythm basics

--- webpack-config.common.js
+++ webpack-config.common.js
@@ -47,7 +47,15 @@ module.exports = {
                 use: [
                     'style-loader',
                     'css-loader',
-                    'sass-loader'
+                    {
+                        loader: 'resolve-url-loader'
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        }
+                    }
                 ]
             },
             {
